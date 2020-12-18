3f29edb2e27730b51e38878c7f665d0a008ce435
Sean Duplantis <sduplantis18@gmail.com>
2020-12-18 17:04:46 -0500

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -129,15 +129,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # My settings
 LOGIN_URL = 'users:login'
