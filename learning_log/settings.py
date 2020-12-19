5bccc1a690548f3ad2c898f95aae91d61343559b
Sean Duplantis <sduplantis18@gmail.com>
2020-12-19 15:49:00 -0500

    test

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Seated',
+        'USER': 'postgres',
+        'PASSWORD':'Baller21',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
