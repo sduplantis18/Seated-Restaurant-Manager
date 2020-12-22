52b3de6bf947478d4ed934beca56fd59307395c7
Sean Duplantis <sduplantis18@gmail.com>
2020-12-22 16:52:12 -0500

    aws s3 config

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -165,3 +166,11 @@ django_heroku.settings(locals())
 # Media Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#AWS S3 settings
+AWS_ACCESS_KEY_ID = 'AKIAUL6MJAMGLRCSPNTV'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'seated-bucket'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
